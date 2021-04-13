56dc1460603916f239f76410320ab85bd5dc1c17
ZogeMung <1533567830@qq.com>
2021-04-13 22:56:19 +0800

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -3,17 +3,16 @@ $(function() {
         $.ajax({
             type: 'post',
             url: 'http://cx.icodef.com/wyn-nb?v=2',
-            // contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
+            contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
             headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Cache-Control': 'no-cache',
-                'max-age': '0',
-                'Authorization': ''
+                // 'Cache-Control': 'no-cache',
+                // 'max-age': '0',
+                // 'Authorization': ''
             },
             data: {
                 'question': $('.question_input').val()
             },
-            crossdomin: true,
+            // crossdomin: true,
             success: function(data) {
                 console.log(data);
                 $(".answer_output").html(data.data);
